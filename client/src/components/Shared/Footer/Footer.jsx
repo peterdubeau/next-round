fd1c4132f1162081743e928806c5eab5947e43e6
Pete Du Beau <peterdubeau@yahoo.com>
2020-08-25 09:25:48 -0400

    minor CSS changes and contact info added to footer

--- client/src/components/Shared/Footer/Footer.jsx
+++ client/src/components/Shared/Footer/Footer.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import { withRouter } from 'react-router-dom'
+import './Footer.css'
 
-function Footer(props) {
+export default function Footer() {
   return (
     <div>
-      
+      <ul className="footer">
+        <li className="contact" id="name">Peter Du Beau 2020</li>
+        <div className="icon-container">
+          <li className="contact"><a href="https://github.com/peterdubeau"><img className="footer-icons" src="https://res.cloudinary.com/dyrvlnond/image/upload/v1598307716/Rookies%20and%20Dragons/github-icon-logo-png-transparent_i2ytsp.png"/></a></li>  
+          <li className="contact"><a href="https://www.linkedin.com/in/peterdubeau"><img className="footer-icons" src="https://www.rpost.com/wp-content/uploads/2016/06/Linkedin_circle.svg_.png"/></a></li>
+        </div>
+      </ul>
     </div>
   )
 }
-
-export default withRouter(Footer)
